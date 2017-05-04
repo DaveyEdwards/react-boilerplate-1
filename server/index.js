d8def679fdf43c21b97c61de2b024f64508692e2
Davey <davey@myiworlds.com>
2017-05-03 21:41:50 -0700

    Not working.  Started setting up server and package.json
    Not happy with hacky way I have it building the schema (it is not actually building right now even though it says it has).  Had issue with using imports, and still do inside the graphql.js file. Not sure why
    
    Need to review server/index.js and possibly pull out graphql content and put in middleware?
    
    Need to edit the npm start command to work in graphql building its schema and something to do with build file

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
+const graphQLHTTP = require('express-graphql');
+const schema = require('./data/schema');
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
@@ -25,12 +27,40 @@ const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 
 const port = argv.port || process.env.PORT || 3000;
+const graphqlPort = 8000;
 
 // Start your app.
 app.listen(port, host, (err) => {
   if (err) {
     return logger.error(err.message);
   }
+
+  // Launch GraphQL
+  const graphql = express();
+  graphql.use('/', graphQLHTTP({
+    graphiql: true,
+    pretty: true,
+    schema
+  }));
+  graphql.listen(graphqlPort, () => console.log(chalk.green(`GraphQL is listening on port ${graphqlPort}`)));
+
+  // Launch Relay by using webpack.config.js
+  const relayServer = new WebpackDevServer(webpack(webpackConfig), {
+    contentBase: '/build/',
+    proxy: {
+      '/graphql': `http://localhost:${graphqlPort}`
+    },
+    stats: {
+      colors: true
+    },
+    hot: true,
+    historyApiFallback: true
+  });
+
+  // Serve static resources
+  relayServer.use('/', express.static(path.join(__dirname, '../build')));
+  relayServer.listen(config.port, () => console.log(chalk.green(`Relay is listening on port ${port}`)));
+
 
   // Connect to ngrok in dev mode
   if (ngrok) {
